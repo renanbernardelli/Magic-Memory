badf725dd6c323cab8a19e9a0a9c595551bf2e9e
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-17 08:24:09 +0100

    :construction: Creating the Card Grid and styling it. :lipstick:

--- src/App.js
+++ src/App.js
@@ -32,6 +32,19 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={ shuffleCards }>New Game</button>
+
+      <div className="card-grid">
+        {
+          cards.map(card => (
+            <div className="card" key={card.id}>
+              <div>
+                <img src={card.src} alt="card front" className="front" />
+                <img src="/img/cover.png" alt="card back" className="back" />
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
